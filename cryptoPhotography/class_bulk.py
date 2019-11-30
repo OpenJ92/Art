59d52882bbfffbf21eca90096341fe365b8d2bd5
OpenJ92 <jacob.vartuli.92@gmail.com>
2019-11-29 21:03:46 -0500

    space edits to satisfy shorter lines in the file.

--- cryptoPhotography/class_bulk.py
+++ cryptoPhotography/class_bulk.py
@@ -2,59 +2,42 @@ import string
 import numpy as np
 
 class MASK_BULK():
-    def __init__(self, textInstance, imageInstance):
-        self.textInstance = textInstance
-        self.imageInstance = imageInstance
-        self.printable = string.ascii_letters
-        self.translateBULKLOC_ = {self.printable[i] : i for i in range(len(self.printable))}
+    def __init__(self, tI, iI):
+        self.tI = tI
+        self.iI = iI
+        self.p = string.ascii_letters
+        self.trB = {self.p[i] : i for i in range(len(self.p))}
         self.BULK_ = self.constructBULK_([16,16])
 
     def constructBULK(self):
-        BULK_ = np.zeros(shape = (self.imageInstance.image.shape[0], self.imageInstance.image.shape[1], 3, len(self.printable))).astype('uint8')
+        BULK_=np.zeros(shape=(self.iI.image.shape[0],self.iI.image.shape[1],3,len(self.p))).astype('uint8')
 
         for i in range(BULK_.shape[0]):
             for j in range(BULK_.shape[1]):
-                letter = next(self.textInstance.cycletext)
+                letter = next(self.tI.cycletext)
                 try:
-                    BULK_[i,j, :, self.translateBULKLOC_[letter]] = np.array([1,1,1])
+                    BULK_[i,j, :, self.trB[letter]] = np.array([1,1,1])
                 except:
                     pass
         return BULK_
 
-    def constructBULK_(self, window_size):
-        BULK_ = np.zeros(shape=(self.imageInstance.image.shape[0], self.imageInstance.image.shape[1], 3, len(self.printable))).astype('uint8')
-        window_bulk = np.ones(shape=(window_size[0], window_size[1], 3))
+    def constructBULK_(self, w_s):
+        BULK_=np.zeros(shape=(self.iI.image.shape[0],self.iI.image.shape[1],3,len(self.p))).astype('uint8')
+        window_bulk = np.ones(shape=(w_s[0], w_s[1], 3))
 
-        for i in window_size[0] * np.arange(BULK_.shape[0] // window_size[0]):
-            for j in window_size[1] * np.arange(BULK_.shape[1] // window_size[1]):
-                letter = next(self.textInstance.cycletext)
+        for i in w_s[0] * np.arange(BULK_.shape[0] // w_s[0]):
+            for j in w_s[1] * np.arange(BULK_.shape[1] // w_s[1]):
+                letter = next(self.tI.cycletext)
                 try:
-                    BULK_[i:i + window_size[0], j:j + window_size[1], :, self.translateBULKLOC_[letter]] = window_bulk
+                    BULK_[i:i+w_s[0],j:j+w_s[1],:,self.trB[letter]]=window_bulk
                 except Exception as e:
-                    BULK_[i:i + window_size[0], j:j + window_size[1], :, np.random.randint(len(self.printable))] = window_bulk
+                    BULK_[i:i+w_s[0],j:j+w_s[1],:,np.random.randint(len(self.p))]=window_bulk
         return BULK_
 
-    def plot_MASK_BULK(self):
-        import matplotlib.pyplot as plt
-        from mpl_toolkits.mplot3d import Axes3D
-
-        bulk = self.BULK_
-        voxels = np.stack([bulk[:,:,0,element] for element in range(bulk.shape[3])])
-        
-        fig = plt.figure(figsize = (18, 12))
-        ax = fig.gca(projection='3d')
-        ax.set_axis_off()
-        print("Before Voxel Plot")
-        ax.voxels(voxels, facecolors='red', edgecolor = 'k')
-        plt.show()
-        print("After Voxel Plot")
-
-        import pdb;pdb.set_trace();
-
 class TEMPORALimg_BULK():
-    def __init__(self, imageInstance):
-        self.printable = string.ascii_letters
-        self.BULK_ = np.zeros(shape=(imageInstance.image.shape[0], imageInstance.image.shape[1], 3,len(self.printable))).astype('uint8')
+    def __init__(self, iI):
+        self.p = string.ascii_letters
+        self.BULK_ = np.zeros(shape=(iI.image.shape[0], iI.image.shape[1], 3,len(self.p))).astype('uint8')
 
     def updateBULK_(self, frame):
         self.BULK_ = np.roll(self.BULK_, 1, axis = 3)
@@ -65,9 +48,9 @@ class TEMPORALimg_BULK():
         self.BULK_[:, :, :, counter % len_] = frame
 
 class TEMPORALimg_BULK_Omega():
-    def __init__(self, imageInstance):
-        self.printable = string.ascii_letters
-        self.BULK_ = np.zeros(shape=(imageInstance.image.shape[0], imageInstance.image.shape[1], 3,len(self.printable))).astype('uint8')
+    def __init__(self, iI):
+        self.p = string.ascii_letters
+        skelf.BULK_ = np.zeros(shape=(iI.image.shape[0], iI.image.shape[1], 3,len(self.p))).astype('uint8')
 
     def updateBULK_(self, frameR, frameB, frameG):
         frame = np.stack([frameR[:,:,0], frameB[:,:,1], frameG[:,:,2]], axis = 2)
@@ -80,7 +63,7 @@ class TEMPORALimg_BULK_Omega():
 
 
 class STATICimg_BULK():
-    def __init__(self, imageInstance):
+    def __init__(self, iI):
         pass
 
 if __name__ == "__main__":
